42fa07e1577eaf20fb7187ac7c298c86aaf60855
Joao Pedro Soares <jpos99@hotmail.com>
2023-08-10 08:32:45 -0300

    Class 1 'Simple spider'

--- quoutes_spyder/quoutes_spyder/settings.py
+++ quoutes_spyder/quoutes_spyder/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "quoutes_spyder.spiders"
 #USER_AGENT = "quoutes_spyder (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
